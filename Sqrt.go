97cc5d35706643ac2d33ee22673d3b59123ab7b9
ramazankilimci <ramazankilimci@gmail.com>
2020-10-07 08:03:54 +0300

    Added custom Sqrt func and error handling.

---
+++ Sqrt.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	z := float64(1)
+	i := 0
+	for i < 10 {
+		z -= (z*z - x) / (2 * z)
+		i++
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
